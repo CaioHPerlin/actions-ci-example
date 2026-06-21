256b5fadcaacd80b52f139813135264fbaf84041
caiohperlin <caiohperlin@gmail.com>
2026-06-20 22:48:18 -0400

    feat: add multiplication

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -1,5 +1,9 @@
-const { somar } = require("./calculadora");
+const { somar, multiplicar } = require("./calculadora");
 
 test("Deve somar 2 + 3 e retornar 5", () => {
   expect(somar(2, 3)).toBe(5);
+});
+
+test("Deve multiplicar 2 * 3 e retornar 6", () => {
+  expect(multiplicar(2, 3)).toBe(6);
 });
